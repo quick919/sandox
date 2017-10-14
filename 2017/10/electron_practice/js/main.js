ee0ad43796442c5024a069045f8fdf10ece77a6d
quick919 <qquick919@gmail.com>
2017-10-14 17:29:56 +0900

    add text.

--- 2017/10/electron_practice/js/main.js
+++ 2017/10/electron_practice/js/main.js
@@ -1,5 +1,34 @@
 var main = {};
 
-(function () {
+(function() {
+  var addTaskElement = document.querySelector("#addNewTask");
+  addTaskElement.addEventListener("click", addTask);
 
-}(main))
+  function addTask() {
+    var newTaskTextElement = document.querySelector("#newTaskText");
+    var newTaskText = newTaskTextElement.value;
+    if (newTaskText == "") {
+      alert("task is empty!");
+      return;
+    }
+
+    var fibonacciNumberElement = document.querySelector("#fibonacciNumber");
+    var fibonacciValue = fibonacciNumberElement.value;
+    var ul = document.createElement("ul");
+    var changedUl = createtTaskElement(ul, newTaskText, fibonacciValue);
+    var taskArea = document.querySelector("#taskArea");
+    taskArea.appendChild(changedUl);
+  }
+
+  function createtTaskElement(ul, task, fibonacciNumber) {
+    var li = document.createElement("ll");
+    var label = document.createElement("label");
+    var p = document.createElement("p");
+    label.innerText = "fibonacci" + fibonacciNumber;
+    p.innerText = task;
+    li.appendChild(label);
+    li.appendChild(p);
+    ul.appendChild(li);
+    return ul;
+  }
+})(main);
