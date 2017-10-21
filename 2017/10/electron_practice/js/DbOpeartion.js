21cff885174a53dd726e103d30556c9677f66260
quick919 <qquick919@gmail.com>
2017-10-21 10:03:32 +0900

    add delete button.

--- 2017/10/electron_practice/js/DbOpeartion.js
+++ 2017/10/electron_practice/js/DbOpeartion.js
@@ -3,6 +3,7 @@ var DbOperation = {};
 (function(ns) {
   ns.insert = insert;
   ns.fetchInitialData = fetchInitialData;
+  ns.deleteObj = deleteObj;
   ns.objects = "";
 
   var Datastore = require("nedb");
@@ -25,5 +26,9 @@ var DbOperation = {};
   function fetchInitialData() {
     setTimeout(function() {}, 1000);
     return DbOperation.objects;
+  }
+
+  function deleteObj(objId) {
+    db.remove({ id: objId }, {}, function(err, numRemoved) {});
   }
 })(DbOperation);
--- 2017/10/electron_practice/js/main.js
+++ 2017/10/electron_practice/js/main.js
@@ -4,6 +4,7 @@ var main = {};
   var addTaskElement = document.querySelector("#addNewTask");
   addTaskElement.addEventListener("click", addTask);
   inittialize();
+  const shortId = require("shortid");
 
   function inittialize() {
     setTimeout(fetchInitialData, 100);
@@ -15,7 +16,7 @@ var main = {};
 
     var ul = document.createElement("ul");
     objects.forEach(function(obj) {
-      createtTaskElement(ul, obj.task, obj.fibonacci);
+      createtTaskElement(ul, obj.id, obj.task, obj.fibonacci);
     });
     var taskArea = document.querySelector("#taskArea");
     taskArea.appendChild(ul);
@@ -29,28 +30,44 @@ var main = {};
       return;
     }
 
+    var id = shortId.generate();
     var fibonacciNumberElement = document.querySelector("#fibonacciNumber");
     var fibonacciValue = fibonacciNumberElement.value;
-    var ul = document.createElement("ul");
-    var changedUl = createtTaskElement(ul, newTaskText, fibonacciValue);
-    var taskArea = document.querySelector("#taskArea");
     var obj = {
+      id: id,
       fibonacci: fibonacciValue,
       task: newTaskText
     };
     DbOperation.insert(obj);
+
+    var ul = document.createElement("ul");
+    var changedUl = createtTaskElement(ul, id, newTaskText, fibonacciValue);
+    var taskArea = document.querySelector("#taskArea");
     taskArea.appendChild(changedUl);
   }
 
-  function createtTaskElement(ul, task, fibonacciNumber) {
+  function createtTaskElement(ul, id, task, fibonacciNumber) {
     var li = document.createElement("ll");
     var label = document.createElement("label");
     var p = document.createElement("p");
+    var input = document.createElement("input");
+
     label.innerText = "fibonacci" + fibonacciNumber;
     p.innerText = task;
+    input.type = "button";
+    input.onclick = function() {
+      deleteTask(id);
+    };
+    input.value = "delete";
+
     li.appendChild(label);
     li.appendChild(p);
+    li.appendChild(input);
     ul.appendChild(li);
     return ul;
+  }
+
+  function deleteTask(id) {
+    DbOperation.deleteObj(id);
   }
 })(main);
