3d3c472765dd9e24fe92fc1232a20e551e1e8886
quick919 <qquick919@gmail.com>
2017-12-29 06:46:09 +0900

    3end

--- 2017/12/testdriven_ruby/Dollar.rb
+++ 2017/12/testdriven_ruby/Dollar.rb
@@ -7,4 +7,8 @@ class Dollar
   def times(multiplier)
     return Dollar.new(@amount * multiplier)
   end
+
+  def equal?(dollar)
+    return @amount == dollar.amount
+  end
 end
--- 2017/12/testdriven_ruby/MoneyTest.rb
+++ 2017/12/testdriven_ruby/MoneyTest.rb
@@ -9,4 +9,9 @@ class MoneyTest < Test::Unit::TestCase
     product = five.times(3)
     assert_equal 15, product.amount
   end
+
+  def test_equals
+    assert_true Dollar.new(5).equal?(Dollar.new(5))
+    assert_false Dollar.new(5).equal?(Dollar.new(6))
+  end
 end
