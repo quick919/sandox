408cef9eac970ad28ff2f07fa3d5c417a3d98c2d
quick919 <qquick919@gmail.com>
2017-12-30 07:27:12 +0900

    7end

--- 2017/12/testdriven_ruby/Money.rb
+++ 2017/12/testdriven_ruby/Money.rb
@@ -2,6 +2,6 @@ class Money
   attr_accessor :amount
 
   def ==(other_money)
-    @amount == other_money.amount
+    @amount == other_money.amount && self.class.equal?(other_money.class)
   end
 end
