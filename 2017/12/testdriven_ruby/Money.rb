b76d217fab8fb0517cc0cd7f05e7e0a81ee3a4a8
quick919 <qquick919@gmail.com>
2017-12-30 17:57:10 +0900

    8end

--- 2017/12/testdriven_ruby/Money.rb
+++ 2017/12/testdriven_ruby/Money.rb
@@ -4,4 +4,15 @@ class Money
   def ==(other_money)
     @amount == other_money.amount && self.class.equal?(other_money.class)
   end
+  
+  def self.dollar(amount)
+    return Dollar.new(amount)
+  end
+
+  def self.franc(amount)
+    return Franc.new(amount)
+  end
+
+  def times(multiplier)
+  end
 end
